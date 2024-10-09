78dafa48fccedce67b7d106179c55907498d91c6
Aymeric Aitamer <aymeric@kohort.eu>
2024-10-09 14:50:21 +0200

    Override coupon code behaviour

--- kohortpay/kohortpay.php
+++ kohortpay/kohortpay.php
@@ -82,84 +82,6 @@ function kohortpay_send_order_to_api($order_id)
 }
 
 /**
- * Get order data to send to KohortPay API
- */
-function getData($order)
-{
-  $order_data = $order->get_data();
-  $line_items = $order->get_items();
-  $shipping_methods = $order->get_shipping_methods();
-  $discounts = $order->get_coupon_codes();
-  $items = [];
-
-  // Products
-  foreach ($line_items as $item) {
-    $product = $item->get_product();
-    $items[] = [
-      'name' => strip_tags($product->get_name()),
-      'description' => strip_tags($product->get_description()),
-      'quantity' => $item->get_quantity(),
-      'price' => cleanPrice($product->get_price()),
-      'image_url' =>
-        wp_get_attachment_image_src(
-          $product->get_image_id(),
-          'shop_thumbnail'
-        )[0] ?? wc_placeholder_img_src(),
-      'type' => 'PRODUCT',
-    ];
-  }
-
-  // Shipping methods
-  foreach ($shipping_methods as $shipping_method) {
-    $items[] = [
-      'name' => $shipping_method->get_method_title(),
-      'quantity' => 1,
-      'price' => cleanPrice(
-        $shipping_method->get_total() + $shipping_method->get_total_tax()
-      ),
-      'type' => 'SHIPPING',
-    ];
-  }
-
-  // Discounts
-  foreach ($discounts as $discount) {
-    $items[] = [
-      'name' => $discount,
-      'quantity' => 1,
-      'price' =>
-        cleanPrice($order->get_discount_total() + $order->get_discount_tax()) *
-        -1,
-      'type' => 'DISCOUNT',
-    ];
-  }
-
-  return [
-    'customerFirstName' => $order_data['billing']['first_name'],
-    'customerLastName' => $order_data['billing']['last_name'],
-    'customerEmail' => $order_data['billing']['email'],
-    'customerPhoneNumber' => $order_data['billing']['phone'],
-    'locale' => get_locale(),
-    'amountTotal' => cleanPrice($order_data['total']),
-    'lineItems' => $items,
-    'metadata' => [
-      'order_id' => $order_data['id'],
-      'customer_id' => $order_data['customer_id'],
-    ],
-  ];
-}
-
-/**
- * Clean price to avoid price with more than 2 decimals.
- */
-function cleanPrice($price)
-{
-  $price = number_format($price, 2, '.', '');
-  $price = $price * 100;
-
-  return $price;
-}
-
-/**
  * Add a new settings tab in WooCommerce
  */
 function kohortpay_add_settings_tab($settings_tabs)
@@ -241,4 +163,205 @@ function kohortpay_get_settings()
   ];
 
   return apply_filters('kohortpay_settings', $settings);
+}
+
+/**
+ * Validate custom KohortPay coupons
+ */
+function kohortpay_validate_coupon($valid, $coupon)
+{
+  $coupon_code = $coupon->get_code();
+
+  // Check if the coupon code starts with "KHTPAY-"
+  if (strpos($coupon_code, 'khtpay-') === 0) {
+    // coupon_code = khtpay-test-f2880fe9
+    // Make it KHTPAY-test-F2880FE9
+    $coupon_code = str_replace('TEST', 'test', strtoupper($coupon_code));
+
+    wc_add_notice(
+      'kohortpay_validate_coupon function called.' . $coupon_code,
+      'notice'
+    );
+
+    // Make an API call to validate the coupon
+    $response = wp_remote_post(
+      'https://api.kohortpay.dev/payment-groups/' . $coupon_code . '/validate',
+      [
+        'headers' => [
+          'Authorization' => 'Bearer ' . get_option('kohortpay_api_secret_key'),
+        ],
+      ]
+    );
+
+    // Check for errors in the API response
+    if (
+      is_wp_error($response) ||
+      wp_remote_retrieve_response_code($response) !== 200
+    ) {
+      // Display an error message
+      $errorResponse = json_decode(wp_remote_retrieve_body($response), true);
+      $errorCode = $errorResponse['error']['code'] ?? 'UNKNOWN_ERROR';
+      wc_add_notice(getErrorMessageByCode($errorCode), 'error');
+
+      // Return false to indicate the coupon is not valid
+      return false;
+    } else {
+      $body = wp_remote_retrieve_body($response);
+      $data = json_decode($body, true);
+
+      $cashbackType = $data['discount_type'] ?? 'PERCENTAGE';
+      $cashbackValue = $data['current_discount_level']['value'] ?? 0.0;
+      $cartTotal = WC()->cart->get_total();
+
+      // If cashback type is percentage, display the cashback amount in amount
+      if ($cashbackType === 'PERCENTAGE') {
+        $cashbackValue = $cartTotal * ($cashbackValue / 100);
+      }
+
+      // Display a success message and indicate the cashback amount
+      wc_add_notice(
+        __('Cashback unlocked:', 'kohortpay') . ' ' . wc_price($cashbackValue),
+        'success'
+      );
+
+      // Return true to indicate the coupon is valid
+      return true;
+    }
+  }
+
+  return $valid;
+}
+add_filter('woocommerce_coupon_is_valid', 'kohortpay_validate_coupon', 99, 2);
+
+/**
+ * Get order data to send to KohortPay API
+ */
+function getData($order)
+{
+  $order_data = $order->get_data();
+  $line_items = $order->get_items();
+  $shipping_methods = $order->get_shipping_methods();
+  $discounts = $order->get_coupon_codes();
+  $items = [];
+
+  // Products
+  foreach ($line_items as $item) {
+    $product = $item->get_product();
+    $items[] = [
+      'name' => strip_tags($product->get_name()),
+      'description' => strip_tags($product->get_description()),
+      'quantity' => $item->get_quantity(),
+      'price' => cleanPrice($product->get_price()),
+      'image_url' =>
+        wp_get_attachment_image_src(
+          $product->get_image_id(),
+          'shop_thumbnail'
+        )[0] ?? wc_placeholder_img_src(),
+      'type' => 'PRODUCT',
+    ];
+  }
+
+  // Shipping methods
+  foreach ($shipping_methods as $shipping_method) {
+    $items[] = [
+      'name' => $shipping_method->get_method_title(),
+      'quantity' => 1,
+      'price' => cleanPrice(
+        $shipping_method->get_total() + $shipping_method->get_total_tax()
+      ),
+      'type' => 'SHIPPING',
+    ];
+  }
+
+  // Discounts
+  foreach ($discounts as $discount) {
+    $items[] = [
+      'name' => $discount,
+      'quantity' => 1,
+      'price' =>
+        cleanPrice($order->get_discount_total() + $order->get_discount_tax()) *
+        -1,
+      'type' => 'DISCOUNT',
+    ];
+  }
+
+  return [
+    'customerFirstName' => $order_data['billing']['first_name'],
+    'customerLastName' => $order_data['billing']['last_name'],
+    'customerEmail' => $order_data['billing']['email'],
+    'customerPhoneNumber' => $order_data['billing']['phone'],
+    'clientReferenceId' => $order_data['id'],
+    'locale' => get_locale(),
+    'amountTotal' => cleanPrice($order_data['total']),
+    'lineItems' => $items,
+    'metadata' => [
+      'order_id' => $order_data['id'],
+      'customer_id' => $order_data['customer_id'],
+    ],
+  ];
+}
+
+/**
+ * Clean price to avoid price with more than 2 decimals.
+ */
+function cleanPrice($price)
+{
+  $price = number_format($price, 2, '.', '');
+  $price = $price * 100;
+
+  return $price;
+}
+
+/**
+ * Manage error messages
+ */
+function getErrorMessageByCode($errorCode)
+{
+  $errorMessage = '';
+  switch ($errorCode) {
+    case 'AMOUNT_TOO_LOW':
+      $errorMessage = __(
+        'The cart amount is too low to use this referral code.',
+        'kohortpay'
+      );
+      break;
+    case 'COMPLETED_EXPIRED_CANCELED':
+      $errorMessage = __(
+        'Unfortunately, the referral period of the kohort has ended.',
+        'kohortpay'
+      );
+      break;
+    case 'MAX_PARTICIPANTS_REACHED':
+      $errorMessage = __(
+        'Unfortunately, the maximum number of people in the kohort has been reached.',
+        'kohortpay'
+      );
+      break;
+    case 'EMAIL_ALREADY_USED':
+      $errorMessage = __(
+        'The email address has already been used to join the kohort.',
+        'kohortpay'
+      );
+      break;
+    case 'NOT_FOUND':
+      $errorMessage = __(
+        'The referral code is unknown or not found.',
+        'kohortpay'
+      );
+      break;
+    default:
+      $errorMessage = __('The referral code is invalid.', 'kohortpay');
+      break;
+  }
+
+  $minimumAmount = wc_price(get_option('kohortpay_minimum_amount'));
+  $defaultSuffixErrorMessage =
+    __('Complete a purchase of at least ', 'kohortpay') .
+    $minimumAmount .
+    __(
+      ' to generate a referral code and get cashback on your order by sharing it.',
+      'kohortpay'
+    );
+
+  return $errorMessage . ' ' . $defaultSuffixErrorMessage;
 }
